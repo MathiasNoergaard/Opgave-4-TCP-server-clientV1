0089335c24338b3bd4af2ddebe544f69ae6c4708
MathiasNoergaard <man010@edu.zealand.dk>
2024-10-10 09:57:37 +0200

    Add project files.

---
+++ TCPclient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Sockets;
+
+string WriteAndReadToServer(StreamWriter writer, StreamReader reader)
+{
+    writer.WriteLine(Console.ReadLine());
+    writer.Flush();
+    string message = reader.ReadLine();
+    Console.WriteLine(message);
+    return message;
+}
+
+Console.WriteLine("Starting TCP Client");
+
+TcpClient socket = new();
+socket.Connect("127.0.0.1", 13000); //connect to localhost
+NetworkStream stream = socket.GetStream();
+StreamReader reader = new(stream);
+StreamWriter writer = new(stream);
+Console.WriteLine("Connected to the TCP server");
+
+string message = null;
+//receive initial message from server
+message = reader.ReadLine();
+Console.WriteLine(message);
+
+//send the command to the server
+do
+{
+    message = WriteAndReadToServer(writer, reader);
+} while (message == "Command not recognized please try again");
+
+//send the values to the server
+string value1;
+string value2;
+do
+{
+    Console.WriteLine("Enter first value");
+    value1 = Console.ReadLine();
+    Console.WriteLine("Enter second value");
+    value2 = Console.ReadLine();
+    writer.WriteLine($"{value1.Trim()} {value2.Trim()}");
+    writer.Flush();
+    message = reader.ReadLine();
+    Console.WriteLine(message);
+} while (message == "One or both values are invalid, please enter two numbers");
+
+socket.Close();
+Console.WriteLine("TCP client closed");
---
+++ TCPserver/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+int CommandLogic(string command, int value1, int value2)
+{
+    switch (command)
+    {
+        case "random":
+            Random randomGenerator = new();
+            return randomGenerator.Next(value1, value2);
+        case "add":
+            return value1 + value2;
+        case "subtract":
+            return value1 - value2;
+    }
+    return 0;
+}
+
+void WriteToClient(StreamWriter writer, string message)
+{
+    writer.WriteLine(message);
+    writer.Flush();
+}
+
+void HandleClient(TcpClient client)
+{
+    NetworkStream stream = client.GetStream();
+    StreamReader reader = new(stream);
+    StreamWriter writer = new(stream);
+    Console.WriteLine("Client connected");
+
+    WriteToClient(writer, "Enter one of following commands: random add subtract");
+
+    string command = reader.ReadLine().ToLower().Trim();
+
+    while (!(command == "random" || command == "add" || command == "subtract"))
+    {
+        WriteToClient(writer, "Command not recognized please try again");
+        command = reader.ReadLine().ToLower().Trim();
+        Console.WriteLine($".{command}.");
+    }
+
+    WriteToClient(writer, "Enter two numbers");
+
+    string[] values = reader.ReadLine().Split(' ');
+    int value1;
+    int value2;
+    while (Int32.TryParse(values[0], out value1) == false ||
+          Int32.TryParse(values[1], out value2) == false)
+    {
+        WriteToClient(writer, "One or both values are invalid, please enter two numbers");
+        values = reader.ReadLine().Split(' ');
+    }
+
+    WriteToClient(writer, $"{CommandLogic(command, value1, value2)}");
+
+    client.Close();
+    Console.WriteLine("Closed connection with client");
+}
+
+Console.WriteLine("Starting TCP server");
+TcpListener listener = new(IPAddress.Any, 13000);
+listener.Start();
+Console.WriteLine("TCP server started");
+
+while (true)
+{
+    TcpClient socket = listener.AcceptTcpClient();
+    Thread thread = new(() => HandleClient(socket));
+    thread.Start();
+}
+
+listener.Stop();
+Console.WriteLine("Server stopped");
